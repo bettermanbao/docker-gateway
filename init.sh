56d90c101eec3607ba844a3e2642e8515ae70d1b
bettermanbao <17328315+bettermanbao@users.noreply.github.com>
2019-01-12 00:20:09 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ sed -i s/\"port\":port,/\"port\":$2,/ /v2ray/config.json
 sed -i s/\"method\":\"method\",/\"method\":\"$3\",/ /v2ray/config.json
 sed -i s/\"password\":\"password\",/\"password\":\"$4\",/ /v2ray/config.json
 
-/v2ray
+/v2ray/v2ray
