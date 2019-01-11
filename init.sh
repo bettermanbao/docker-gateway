6d4ae4e55632ef36eff3da458ebd2a3688d1fc25
bettermanbao <17328315+bettermanbao@users.noreply.github.com>
2019-01-12 00:22:23 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,6 @@ sed -i s/\"port\":port,/\"port\":$2,/ /v2ray/config.json
 sed -i s/\"method\":\"method\",/\"method\":\"$3\",/ /v2ray/config.json
 sed -i s/\"password\":\"password\",/\"password\":\"$4\",/ /v2ray/config.json
 
+iptables -t nat -I PREROUTING -p tcp ! -d $(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}') -j REDIRECT --to-port 1081
+
 /v2ray/v2ray
