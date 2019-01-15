d43a916d22e6a7c44e6833a6fae1d318ef97406b
bettermanbao <17328315+bettermanbao@users.noreply.github.com>
2019-01-15 22:21:03 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# init.sh address port method password
-
-sed -i s/\"address\":\"address\",/\"address\":\"$1\",/ /v2ray/config.json
-sed -i s/\"port\":port,/\"port\":$2,/ /v2ray/config.json
-sed -i s/\"method\":\"method\",/\"method\":\"$3\",/ /v2ray/config.json
-sed -i s/\"password\":\"password\",/\"password\":\"$4\",/ /v2ray/config.json
-
 echo "nameserver 114.114.114.114" > /etc/resolv.conf
 iptables -t nat -I PREROUTING -p tcp ! -d $(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}') -j REDIRECT --to-port 1081
 
+/root/ssr-local -c /root/config.json -u  > /dev/null 2>&1 &
 /v2ray/v2ray > /dev/null 2>&1
