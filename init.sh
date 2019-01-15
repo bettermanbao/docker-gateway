6af08e6b582e8959fffde27892222e2f04245e35
bettermanbao <17328315+bettermanbao@users.noreply.github.com>
2019-01-15 22:40:50 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 echo "nameserver 114.114.114.114" > /etc/resolv.conf
 iptables -t nat -I PREROUTING -p tcp ! -d $(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}') -j REDIRECT --to-port 1081
 
-/root/ssr-local -c /root/config.json -u  > /dev/null 2>&1 &
+/ssr-local -c /root/config.json -u  > /dev/null 2>&1 &
 /v2ray/v2ray > /dev/null 2>&1
